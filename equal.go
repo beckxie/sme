15599ebe7737092c259f67b7aa03d7d6a97c2c5a
Beck <20741391+beckxie@users.noreply.github.com>
2021-04-02 02:58:55 +0800

    add early return

--- equal.go
+++ equal.go
@@ -4,6 +4,13 @@ import "sync"
 
 // IsEqual compare two sync.Map is equal or not.
 func IsEqual(mu *sync.Mutex, a *sync.Map, b *sync.Map) bool {
+	switch {
+	case a == b:
+		return true
+	case a == nil || b == nil:
+		return false
+	}
+
 	if mu == nil {
 		mu = &sync.Mutex{}
 	}
