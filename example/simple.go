bfecd993ee1f942f31809a67929e86f90ce8c7b8
Beck <20741391+beckxie@users.noreply.github.com>
2021-04-02 01:29:20 +0800

    add more example

--- example/simple.go
+++ example/simple.go
@@ -18,6 +18,12 @@ func main() {
 	b.Store("2", 2)
 	b.Store("3", "3")
 
+	c := &sync.Map{}
+	c.Store(1, nil)
+	c.Store("2", 2)
+	c.Store("3", 3)
+
 	var mu sync.Mutex
-	fmt.Printf("Simple compare two sync.Map result: %t\n", sme.IsEqual(&mu, a, b))
+	fmt.Printf("Simple compare sync.Map-a and sync.Map-b result: %t\n", sme.IsEqual(&mu, a, b))
+	fmt.Printf("Simple compare sync.Map-a and sync.Map-c result: %t\n", sme.IsEqual(&mu, a, c))
 }
